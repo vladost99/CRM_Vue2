44ec10d5c88876917f97ddcf35a0c6359e9b0ed8
Vladyslav <vladsa000@gmail.com>
2021-02-25 16:39:28 +0200

    add vue chart in history page

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,7 @@
   </div>
 
   <div class="history-chart">
-    <canvas></canvas>
+    <canvas ref="canvas"></canvas>
   </div>
     
     <Loader v-if="loading" />
@@ -32,11 +32,13 @@
 </div>
 </template>
 <script>
+import {Pie} from 'vue-chartjs';
 import HistoryTable from '../components/HistoryTable';
 import Loader from '../components/Loader.vue';
 import paginationMixin from '../mixins/paginationMixin';
 export default {
   name: 'history',
+  extends: Pie,
   components: {
     HistoryTable,
     Loader
@@ -50,9 +52,13 @@ export default {
   },
   async mounted() {
         this.records = await this.$store.dispatch('fetchRecords');
-       
         const categories = await this.$store.dispatch('fetchCategories');
 
+        this.setup(categories);
+        this.loading = false;
+  },
+  methods: {
+    setup(categories) {
         this.setupPagination(this.records.map(record => {
             return {
               ...record,
@@ -61,7 +67,38 @@ export default {
               typeText: record.type === 'income' ? 'Доход': 'Расход',
             }
         }));
-        this.loading = false;
+        this.renderChart({
+        labels: categories.map(c => c.title),
+        datasets: [{
+            label: 'Расходы по катогориям',
+            data: categories.map(c => {
+              return this.records.reduce((total, r) => {
+                if (r.categoryId === c.id && r.type === 'outcome') {
+                  total += +r.amount;
+                }
+                return total;
+              } ,0);
+            }),
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+      });
+    }
   }
 }
 </script>
